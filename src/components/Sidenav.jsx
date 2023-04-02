b9302c88432e8af2fb1c9cd8e1aef12ac19743b1
BaovolaRomy <baoromy@icloud.com>
2023-04-02 12:23:56 +0200

    modification

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -13,10 +13,10 @@ const Sidenav =() => {
 
     return(
         <div>
-           <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden' />
+           <AiOutlineMenu onClick={handleNav} className=' top-4 right-4 z-[99] absolute' />
            { nav ? (
                 <>
-                {/* <RxCross2 onClick={handleNav} className='absolute top-4 right-4 z-[99]' /> */}
+                
                 <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
                     <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
                         <AiOutlineHome size={20} />
@@ -52,6 +52,8 @@ const Sidenav =() => {
                         <AiOutlineMail size={20} />
                         <span className="pl-4">Contact</span>
                     </a>
+                    
+                    <RxCross2 onClick={handleNav} className='absolute top-4 right-4 z-[99]' />
 
                 </div></>
            ) : (
